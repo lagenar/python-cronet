5dc26dc9812f3a76c737e1dbafac2560ad49f111
Lucas Moauro <lagenar@gmail.com>
2024-01-30 16:34:14 -0300

    Add sample code

--- experiments/build.sh
+++ experiments/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc -I/home/lucas/chromium/src/components/cronet/out/Cronet/cronet/include main.c -L/home/lucas/chromium/src/components/cronet/out/Cronet/ -lcronet.123.0.6270.0 -o cronet
+g++ -I/home/lucas/chromium/src/components/cronet/out/Cronet/cronet/include main.cc sample_executor.cc sample_url_request_callback.cc -L/home/lucas/chromium/src/components/cronet/out/Cronet/ -lcronet.123.0.6270.0 -o cronet
 
 LD_LIBRARY_PATH=/home/lucas/chromium/src/components/cronet/out/Cronet/:$LD_LIBRARY_PATH ./cronet
--- experiments/main.c
+++ experiments/main.c
@@ -1,7 +1,64 @@
 
 #include <stdbool.h>
 #include "cronet_c.h"
+#include <unistd.h>
+#include <stdio.h>
 
+void execute(Cronet_ExecutorPtr executor, Cronet_RunnablePtr runnable)
+{
+    printf("execute");
+    Cronet_Runnable_Run(runnable);
+    Cronet_Runnable_Destroy(runnable);
+}
+
+
+void on_redirect_received(Cronet_UrlRequestCallbackPtr self, Cronet_UrlRequestPtr request, Cronet_UrlResponseInfoPtr info, Cronet_String new_location_url)
+{
+
+}
+
+void on_response_started(Cronet_UrlRequestCallbackPtr self, Cronet_UrlRequestPtr request, Cronet_UrlResponseInfoPtr info)
+{
+    printf("called started");
+}
+
+
+void on_read_completed(Cronet_UrlRequestCallbackPtr self, Cronet_UrlRequestPtr request, Cronet_UrlResponseInfoPtr info, Cronet_BufferPtr buffer, uint64_t bytes_read) 
+{
+    
+}
+
+
+void on_succeeded(Cronet_UrlRequestCallbackPtr self, Cronet_UrlRequestPtr request, Cronet_UrlResponseInfoPtr info)
+{
+
+}
+
+void on_failed(Cronet_UrlRequestCallbackPtr self, Cronet_UrlRequestPtr request, Cronet_UrlResponseInfoPtr info, Cronet_ErrorPtr error)
+{
+
+}
+
+void on_canceled(Cronet_UrlRequestCallbackPtr self, Cronet_UrlRequestPtr request, Cronet_UrlResponseInfoPtr info)
+{
+
+}
+
+
+void perform_request(Cronet_EnginePtr cronet_engine, char* url, Cronet_ExecutorPtr executor) {
+  //SampleUrlRequestCallback url_request_callback;
+  Cronet_UrlRequestCallbackPtr callback = Cronet_UrlRequestCallback_CreateWith(
+    &on_redirect_received, &on_response_started, &on_read_completed, &on_succeeded, &on_failed, &on_canceled);
+  Cronet_UrlRequestPtr request = Cronet_UrlRequest_Create();
+  Cronet_UrlRequestParamsPtr request_params = Cronet_UrlRequestParams_Create();
+  Cronet_UrlRequestParams_http_method_set(request_params, "GET");
+
+  Cronet_UrlRequest_InitWithParams(request, cronet_engine, url, request_params, callback, executor);
+  Cronet_UrlRequestParams_Destroy(request_params);
+
+  Cronet_UrlRequest_Start(request);
+  //Cronet_UrlRequest_Destroy(request);
+}
 
 
 int main(int argc, const char* argv[]) 
@@ -12,9 +69,13 @@ int main(int argc, const char* argv[])
     Cronet_EngineParams_enable_quic_set(engine_params, true);
 
     Cronet_Engine_StartWithParams(cronet_engine, engine_params);
+    Cronet_ExecutorPtr executor = Cronet_Executor_CreateWith(&execute);
+
+    perform_request(cronet_engine, "https://google.com", executor);
+
     Cronet_EngineParams_Destroy(engine_params);
- 
-    Cronet_Engine_Shutdown(cronet_engine);
-    Cronet_Engine_Destroy(cronet_engine);
+    
+    //Cronet_Engine_Shutdown(cronet_engine);
+    //Cronet_Engine_Destroy(cronet_engine);
     return 0;
 }
