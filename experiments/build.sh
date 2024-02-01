55fd18e086dac0c338d80e340a33320ff6ba8e00
Lucas Moauro <lagenar@gmail.com>
2024-02-01 17:22:30 -0300

    Add get function using pybind11

--- experiments/build.sh
+++ experiments/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-g++ -I/home/lucas/chromium/src/components/cronet/out/Cronet/cronet/include main.cc sample_executor.cc sample_url_request_callback.cc -L/home/lucas/chromium/src/components/cronet/out/Cronet/ -lcronet.123.0.6270.0 -o cronet
 
-LD_LIBRARY_PATH=/home/lucas/chromium/src/components/cronet/out/Cronet/:$LD_LIBRARY_PATH ./cronet
+include_dir="$HOME/chromium/src/components/cronet/out/Cronet/cronet/include"
+lib_dir="$HOME/chromium/src/components/cronet/out/Cronet/"
+
+c++ -I"$include_dir" -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) example.cpp sample_executor.cc sample_url_request_callback.cc -L"$lib_dir" -lcronet.123.0.6270.0 -o example$(python3-config --extension-suffix)
+LD_LIBRARY_PATH=$lib_dir:$LD_LIBRARY_PATH python -c 'import example; print(example.get("https://example.com"))'
--- experiments/cronet.md
+++ experiments/cronet.md
@@ -19,11 +19,14 @@ ninja -C out/Cronet cronet_package
 - c++ example: https://chromium.googlesource.com/chromium/src/+/refs/heads/main/components/cronet/native/sample/main.cc
 - Java API: https://chromium.googlesource.com/chromium/src/+/HEAD/components/cronet/README.md
 
-# Cython
+# c++
 
-- c++: https://cython.readthedocs.io/en/latest/src/userguide/wrapping_CPlusPlus.html
+- https://github.com/pybind/pybind11
 
+# 2024-02-01
 
-/home/lucas/chromium/src/components/cronet/out/Cronet/cronet
-/home/lucas/chromium/src/net/out/Cronet
-/home/lucas/chromium/src/components/cronet/out/Cronet/cronet/include
+- Cython script that gets a url response using cronet
+
+get
+post
+user-agent
