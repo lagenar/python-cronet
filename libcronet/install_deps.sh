427a3fba88c25c6c1810d7d3c64ee226a01b043e
Lucas Moauro <lagenar@gmail.com>
2024-03-25 10:54:02 -0300

    Move the installation of yum dependencies to a script

---
+++ libcronet/install_deps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+yum -y install git bzip2 tar pkgconfig atk-devel alsa-lib-devel \
+bison binutils brlapi-devel bluez-libs-devel bzip2-devel cairo-devel \
+cups-devel dbus-devel dbus-glib-devel expat-devel fontconfig-devel \
+freetype-devel gcc-c++ glib2-devel glibc.i686 gperf glib2-devel \
+gtk3-devel java-1.*.0-openjdk-devel libatomic libcap-devel libffi-devel \
+libgcc.i686 libjpeg-devel libstdc++.i686 libX11-devel libXScrnSaver-devel \
+libXtst-devel libxkbcommon-x11-devel ncurses-compat-libs nspr-devel nss-devel \
+pam-devel pango-devel pciutils-devel pulseaudio-libs-devel zlib.i686 httpd \
+mod_ssl php php-cli xorg-x11-server-Xvfb
