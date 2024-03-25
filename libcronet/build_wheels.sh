bfdb2776293ba4922ef0621c5557906e2a8a3d56
Lucas Moauro <lagenar@gmail.com>
2024-03-25 13:26:32 -0300

    Use patched version of auditwheel so that libgobject and libglib are included

--- libcronet/build_wheels.sh
+++ libcronet/build_wheels.sh
@@ -9,18 +9,28 @@ mv /app/cronet_build /tmp
 cp /tmp/cronet_build/libcronet*so /lib64
 cp /tmp/cronet_build/include/*.h /usr/local/include
 
+python3.8 -m pip uninstall --no-input auditwheel
+python3.8 -m pip install git+https://github.com/lagenar/auditwheel.git
 python3.8 -m build
 auditwheel repair --plat manylinux_2_28_x86_64 dist/cronet-$VERSION-cp38-cp38-linux_x86_64.whl
 
+python3.9 -m pip uninstall --no-input auditwheel
+python3.9 -m pip install git+https://github.com/lagenar/auditwheel.git
 python3.9 -m build
 auditwheel repair --plat manylinux_2_28_x86_64 dist/cronet-$VERSION-cp39-cp39-linux_x86_64.whl
 
+python3.10 -m pip uninstall --no-input auditwheel
+python3.10 -m pip install git+https://github.com/lagenar/auditwheel.git
 python3.10 -m build
 auditwheel repair --plat manylinux_2_28_x86_64 dist/cronet-$VERSION-cp310-cp310-linux_x86_64.whl
 
+python3.11 -m pip uninstall --no-input auditwheel
+python3.11 -m pip install git+https://github.com/lagenar/auditwheel.git
 python3.11 -m build
 auditwheel repair --plat manylinux_2_28_x86_64 dist/cronet-$VERSION-cp311-cp311-linux_x86_64.whl
 
+python3.12 -m pip uninstall --no-input auditwheel
+python3.12 -m pip install git+https://github.com/lagenar/auditwheel.git
 python3.12 -m build
 auditwheel repair --plat manylinux_2_28_x86_64 dist/cronet-$VERSION-cp312-cp312-linux_x86_64.whl
 
